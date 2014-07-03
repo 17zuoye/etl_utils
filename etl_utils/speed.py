448ad56ae5d0638daaa6511d06b94729bd067cc5
David Chen <mvjome@gmail.com>
2014-07-03 11:50:50 +0800

    merge chars into regexp

--- etl_utils/speed.py
+++ etl_utils/speed.py
@@ -16,7 +16,7 @@ class Speed():
 
     def inspect(self):
         if self.item_count == 0:
-            print "当前还没有还是计数"
+            print "当前还没有计数"
         else:
             c1 = self.item_count / (self.last_clock - self.born_clock).total_seconds()
             print "当前平均速度为1秒处理", c1, "个"
