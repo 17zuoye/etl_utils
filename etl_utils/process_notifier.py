5710f79be763785187537b9b8e9369d7ad96a291
David Chen <mvjome@gmail.com>
2014-08-06 11:22:45 +0800

    精简process_notifier生成iterator

--- etl_utils/process_notifier.py
+++ etl_utils/process_notifier.py
@@ -14,54 +14,54 @@ class ProcessNotifier(object):
 
     def __init__(self, scope, per, msg):
         """ TypeError: __init__() should return None, not 'generator' """
-        if hasattr(scope, 'count') or isinstance(scope, list) or hasattr(scope, 'itervalues') or hasattr(scope, 'iterator'):
-            self.scope = scope
-        else:
-            raise Exception(u"%s should be iteratable", str(scope))
+        self.iterator = None
+        try:
+            self.iterator = scope.iteritems()
+        except:
+            pass
+        try:
+            self.iterator = scope.iterator()
+        except:
+            pass
+        try:
+            self.iterator = iter(scope)
+        except:
+            pass
+        assert self.iterator, (u"%s should be iteratable" % str(scope))
+
         self.current_pid = os.getpid()
         self.per = per
         self.msg = msg
 
         # 兼容 list, dict, mongomock
         # 判断这个对象的属性方法来觉得用len还是count，不能覆盖所有情况，所以这里直接暴力解决。
-        self.scope_count = None
+        self.total_count = None
         for lambda1 in [ lambda : len(scope), lambda : scope.count()]:
-            if self.scope_count is None:
+            if self.total_count is None:
                 try:
-                    self.scope_count = lambda1()
+                    self.total_count = lambda1()
                 except:
                     pass
-        assert self.scope_count is not None
+        assert self.total_count is not None
 
-
-    def iterator(self):
-        try:
-            self.scope = self.scope.iteritems()
-        except:
-            pass
-        try:
-            self.scope = self.scope.iterator()
-        except:
-            pass
-        return self.scope
 
     def generator(self):
         """ Use a Generator to print the inner status when iterate a scope."""
-        base_str = u"[pid %i] %s processing %i records " % (self.current_pid, self.msg, self.scope_count)
+        base_str = u"[pid %i] %s processing %i records " % (self.current_pid, self.msg, self.total_count)
 
         process_count = 0
 
         widgets = ['  ', base_str.encode("UTF-8"), pb.Percentage(), ' ', pb.Bar(),
                    ' ', ItemProcessSpeed()]
 
-        if self.scope_count:
-            self.pbar = pb.ProgressBar(widgets=widgets, maxval=self.scope_count).start()
+        if self.total_count:
+            self.pbar = pb.ProgressBar(widgets=widgets, maxval=self.total_count).start()
 
-        for record in self.iterator():
+        for record in self.iterator:
             process_count += 1
             # Fix grow up when iterator
-            if process_count > self.scope_count: process_count = self.scope_count
-            if self.scope_count: self.pbar.update(process_count)
+            if process_count > self.total_count: process_count = self.total_count
+            if self.total_count: self.pbar.update(process_count)
             yield record
         print
 
